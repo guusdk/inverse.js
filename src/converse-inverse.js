ccf9060fbe202a5994ad09f4c2d1dcea66f88a55
JC Brand <jc@opkode.com>
2017-04-18 20:39:06 +0200

    Some code have been moved to converse-singleton.js

--- src/converse-inverse.js
+++ src/converse-inverse.js
@@ -67,59 +67,6 @@
                 }
             },
 
-            ChatBoxViews: {
-                showChat: function (attrs) {
-                    /* With inVerse, we only have one chat visible at any one
-                     * time. So before opening a chat, we make sure all other
-                     * chats are hidden.
-                     */
-                    var _converse = this.__super__._converse;
-                    var chatbox = this.getChatBox(attrs, true);
-                    if (!attrs.hidden && _converse.connection.authenticated) {
-                        _.each(_converse.chatboxviews.xget(chatbox.get('id')),
-                            function (view) {
-                                if (view.model.get('id') === 'controlbox') {
-                                    return;
-                                }
-                                view.model.save({'hidden': true});
-                            }
-                        );
-                        chatbox.save({'hidden': false});
-                    }
-                    return this.__super__.showChat.apply(this, arguments);
-                }
-            },
-
-            RosterContactView: {
-                openChat: function (ev) {
-                    /* With inVerse, we only have one chat visible at any one
-                     * time. So before opening a chat, we make sure all other
-                     * chats are hidden.
-                     */
-                    _.each(this.__super__._converse.chatboxviews.xget('controlbox'), function (view) {
-                        view.model.save({'hidden': true});
-                    });
-                    this.model.save({'hidden': false});
-                    return this.__super__.openChat.apply(this, arguments);
-                },
-            },
-
-            ChatBoxView: {
-                _show: function (focus) {
-                    /* With inVerse, we only have one chat visible at any one
-                     * time. So before opening a chat, we make sure all other
-                     * chats are hidden.
-                     */
-                    if (!this.model.get('hidden')) {
-                        _.each(this.__super__._converse.chatboxviews.xget(this.model.get('id')), function (view) {
-                            view.hide();
-                            view.model.set({'hidden': true});
-                        });
-                        return this.__super__._show.apply(this, arguments);
-                    }
-                }
-            },
-
             ChatRoomView: {
                 afterShown: function (focus) {
                     /* Make sure chat rooms are scrolled down when opened
